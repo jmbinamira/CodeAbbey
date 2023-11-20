3f0df62393f84248ac6d26c35969d2dab44c0554
jmbinamira <jmlbinamira@gmail.com>
2023-11-20 12:59:28 +0800

    first commit

---
+++ Sum_A+B.cpp
@@ -0,0 +1,15 @@
+/*
+Input: 1 2
+Outpu: 3
+*/
+
+#include <iostream>
+
+int main() {
+    int a, b, sum = 0;
+    printf("\n");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("%d",sum);
+    return 0;
+}
